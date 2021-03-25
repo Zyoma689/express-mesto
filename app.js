caae10400dbebef1a46a024a4a3bcec9b58c0d8d
Artem Ivanov <aie426205@gmail.com>
2021-03-25 17:47:50 +0300

    feat: add temporary authorization solution

--- app.js
+++ app.js
@@ -7,13 +7,21 @@ const userRouter = require('./routes/users');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '605b1a61ad50c1421acd2984',
+  };
+
+  next();
 });
 
 app.use('/users', userRouter);
