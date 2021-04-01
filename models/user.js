3897051124577c543910b3712c8f45733cf8c014
Artem Ivanov <aie426205@gmail.com>
2021-04-01 16:18:38 +0300

    feat: add email and password to the user schema

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -16,6 +17,20 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     required: [true, 'Ссылка на изображение обязательна'],
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: 'Email должен быть валидным',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
   },
 }, {
   versionKey: false,
