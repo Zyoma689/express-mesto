7883d8ce0552caba255e6ecf90e540dd823fd0b3
Artem Ivanov <aie426205@gmail.com>
2021-04-05 13:16:00 +0300

    feat: create a login contoller

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,9 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const { notFoundError, internalServerError, getError } = require('../errors/errors');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
 const notFoundMessage = { message: 'Запрашиваемый пользователь не найден' };
 
 const getUsers = (req, res) => {
@@ -31,14 +34,20 @@ const createUser = (req, res) => {
   const {
     name, about, avatar, email, password,
   } = req.body;
-  User.create({
-    name, about, avatar, email, password,
-  })
-    .then((user) => {
-      res.status(200).send(user);
+  bcrypt.hash(password, 10)
+    .then((hash) => {
+      User.create({
+        name, about, avatar, email, password: hash,
+      })
+        .then((user) => {
+          res.status(200).send(user);
+        })
+        .catch((err) => {
+          getError(res, err, notFoundMessage);
+        });
     })
     .catch((err) => {
-      getError(res, err, notFoundMessage);
+      res.status(500).send(err);
     });
 };
 
@@ -76,10 +85,23 @@ const updateAvatar = (req, res) => {
     });
 };
 
+const login = (req, res) => {
+  const { email, password } = req.body;
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+      res.cookie('jwt', token, { maxAge: 3600000, httpOnly: true });
+    })
+    .catch((err) => {
+      res.status(401).send({ message: err.message });
+    });
+};
+
 module.exports = {
   getUsers,
   getUserById,
   createUser,
   updateUser,
   updateAvatar,
+  login,
 };
