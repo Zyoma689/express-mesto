22104d483566c8a5c18855b6fa4ce9f68ed80fce
Artem Ivanov <aie426205@gmail.com>
2021-04-06 11:52:35 +0300

    feat: protect password hash

--- models/user.js
+++ models/user.js
@@ -32,13 +32,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Пароль обязателен для заполнения'],
     minlength: 8,
+    select: false,
   },
 }, {
   versionKey: false,
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
